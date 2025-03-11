1e3434f3965ab44527e3495a175cd7fd9216d229
AnkittChauhan <ankittrajput.4@gmail.com>
2025-03-11 14:08:13 +0530

    Changed images

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import TaskApp from './TaskApp';
 import './index.css';
+import bgImg from './assets/3Pokemons.jpg';
 
 function App() {
   return (
     <div className="min-h-screen">
-      <img src="./public/3Pokemons.jpg" alt="BG-img" className="absolute -z-10 w-screen h-screen" />
+      <img src={bgImg} alt="BG-img" className="absolute -z-10 w-screen h-screen" />
       <TaskApp />
     </div>
   );
